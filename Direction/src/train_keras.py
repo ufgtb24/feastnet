ce617754b6b97162ac0f60b21d657a3edc57faa6
ufgtb24 <1026773987@qq.com>
2019-06-08 15:05:39 +0800

    单一case可以收敛,改变 batch_size，加快收敛

--- Direction/src/train_keras.py
+++ Direction/src/train_keras.py
@@ -17,7 +17,7 @@ optimizer = tf.train.AdamOptimizer() #1.x
 model=DirectionModel(CHANNELS,coarse_level=C_LEVEL,fc_dim=4)
 # data_gen = Data_Gen('F:/ProjectData/mesh_direction/2aitest/low/npz')
 data_gen = Data_Gen('/home/yu/Documents/project_data/low/npz_test')
-rot_num=8
+rot_num=20
 rf=Rotate_feed(rot_num,data_gen)
 mean_metric = keras.metrics.Mean()
 for epoch in range(100000):
